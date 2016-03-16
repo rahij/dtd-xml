466c20d1c7547de1630ca4ac1ef66d5c1cb2fb0b
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2016-03-16 23:40:27 +0800

    add basic authentication views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -27,5 +26,10 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+  Route::get('auth/login', 'Auth\AuthController@getLogin');
+  Route::post('auth/login', 'Auth\AuthController@postLogin');
+  Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+  Route::get('auth/register', 'Auth\AuthController@getRegister');
+  Route::post('auth/register', 'Auth\AuthController@postRegister');
 });
