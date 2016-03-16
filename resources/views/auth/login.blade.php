caf4fa9264b9a54cde2402f95e32256093c18801
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2016-03-17 01:35:48 +0800

    make login and register views more bootstrap-ish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,25 +3,25 @@
 @section('content')
   <div class="content">
     <form method="POST" action="/auth/login">
-        {!! csrf_field() !!}
+      {!! csrf_field() !!}
 
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
+      <div class="form-group">
+        Email
+        <input class="form-control" type="email" name="email" value="{{ old('email') }}">
+      </div>
 
-        <div>
-            Password
-            <input type="password" name="password" id="password">
-        </div>
+      <div class="form-group">
+        Password
+        <input class="form-control" type="password" name="password" id="password">
+      </div>
 
-        <div>
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
+      <div class="checkbox">
+        <label><input type="checkbox" name="remember">Remember Me</label>
+      </div>
 
-        <div>
-            <button type="submit">Login</button>
-        </div>
+      <div class="form-group">
+        <input type="submit" value="Login" class="btn btn-primary" />
+      </div>
     </form>
   </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,32 @@
-<form method="POST" action="/auth/register">
+@extends('layouts.master')
+
+@section('content')
+  <form method="POST" action="/auth/register">
     {!! csrf_field() !!}
 
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
+    <div class="form-group">
+      Name
+      <input class="form-control" type="text" name="name" value="{{ old('name') }}">
     </div>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+    <div class="form-group">
+      Email
+      <input class="form-control" type="email" name="email" value="{{ old('email') }}">
     </div>
 
-    <div>
-        Password
-        <input type="password" name="password">
+    <div class="form-group">
+      Password
+      <input class="form-control" type="password" name="password">
     </div>
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
+    <div class="form-group">
+      Confirm Password
+      <input class="form-control" type="password" name="password_confirmation">
     </div>
 
-    <div>
-        <button type="submit">Register</button>
+    <div class="form-group">
+      <input type="submit" value="Register" class="btn btn-primary" />
     </div>
-</form>
+  </form>
+
+@endsection
