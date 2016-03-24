1379a2320d2356a9ba19ad5cc851bf42c4d0c181
Jesper Bränn <brann.jesper@gmail.com>
2016-03-24 17:08:42 +0800

    Make DTD parser static and throw exceptions on errors

--- app/Helpers/DTDtoXML.php
+++ app/Helpers/DTDtoXML.php
@@ -10,65 +10,68 @@ use Faker;
 ** $dtd->writeXML();
 ** Returns XML if it was passed a valid DTD, warnings otherwise
 **
-** TODO: Handle errors / warnings.
-** TODO: Make API nicer to use?
+** TODO: Handle warnings?
 ** TODO: Make it possible to specify which elements serves as root element.
 **          (Right now the topmost element in the DTD is always root)
 **
 **/
 
+$loader = require '../vendor/autoload.php';
+
+class DTDException extends Exception { }
+
 class DTDtoXML {
-  private $dtdContent;
-  private $dtdParser;
-  private $domImplementation;
-  private $xmlTree;
-  private $faker;
+  private static $dtdContent;
+  private static $dtdParser;
+  private static $domImplementation;
+  private static $xmlTree;
+  private static $faker;
 
-  public function __construct($dtd) {
-      $this->dtdContent = $dtd;
-      $this->dtdParser = \Soothsilver\DtdParser\DTD::parseText($dtd);
-      $this->faker = Faker\Factory::create();
+  public static function parseDTD($dtd) {
+      self::$dtdContent = $dtd;
+      self::$dtdParser = \Soothsilver\DtdParser\DTD::parseText($dtd);
+      self::$faker = Faker\Factory::create();
 
-
-      if($this->dtdParser->isWellFormedAndValid()) {
-        $this->generateXML();
+      if(self::$dtdParser->isWellFormedAndValid()) {
+        self::generateXML();
+        self::writeXML();
       } else {
-        print_r($this->dtdParser->errors);
+        $errorString = '';
+        foreach(self::$dtdParser->errors as $error) {
+          $errorString .= $error->getMessage() . "\n";
+        }
+        throw new Exception($errorString);
       }
   }
 
-  public function errors() {
-    return $this->dtdParser->errors;
+  private static function generateXML() {
+    $root = self::$dtdParser->elements[key(self::$dtdParser->elements)];
+
+    self::$domImplementation = new DOMImplementation();
+
+    $dtdData = 'data://text/plain;base64,' . base64_encode(self::$dtdContent);
+    $dtd = self::$domImplementation->createDocumentType($root->type, '', $dtdData);
+
+    self::$xmlTree = self::$domImplementation->createDocument(NULL, NULL, $dtd);
+    self::$xmlTree->xmlVersion = '1.0';
+    self::$xmlTree->encoding="UTF-8";
+
+    $rootElement = self::$xmlTree->createElement($root->type);
+    self::$xmlTree->appendChild($rootElement);
+
+    self::addChildren($root, $rootElement);
   }
 
-  public function generateXML() {
-    $root = $this->dtdParser->elements[key($this->dtdParser->elements)];
-
-    $this->domImplementation = new DOMImplementation();
-
-    $dtdData = 'data://text/plain;base64,' . base64_encode($this->dtdContent);
-    $dtd = $this->domImplementation->createDocumentType($root->type, '', $dtdData);
-
-    $this->xmlTree = $this->domImplementation->createDocument(NULL, NULL, $dtd);
-    $this->xmlTree->xmlVersion = '1.0';
-    $this->xmlTree->encoding="UTF-8";
-
-    $rootElement = $this->xmlTree->createElement($root->type);
-    $this->xmlTree->appendChild($rootElement);
-
-    $this->addChildren($root, $rootElement);
-  }
-
-  private function addAttributes($dtdNode, $xmlNode) {
-    $attributes = $this->dtdParser->elements[$dtdNode->type]->attributes;
+  private static function addAttributes($dtdNode, $xmlNode) {
+    $attributes = self::$dtdParser->elements[$dtdNode->type]->attributes;
     foreach($attributes as $attribute) {
       $value = '';
 
       if($attribute->defaultType == '#IMPLIED') {
         if(mt_rand(0, 99) > 49)
-          $value = $this->stringGenerator(1, 1);
+          $value = self::stringGenerator(1, 1);
       } else if($attribute->defaultType == '#REQUIRED') {
-        $value = $this->stringGenerator(1, 1);
+        $value = self::stringGenerator(1, 1);
       } else if($attribute->defaultType == '#FIXED') {
         $value = $attribute->defaultValue;
       } else if(count($attribute->enumeration) > 0) { // Enumeration
@@ -86,36 +89,37 @@ class DTDtoXML {
     }
   }
 
-  private function addChildren($dtdNode, $xmlNode) {
-    $this->addAttributes($dtdNode, $xmlNode);
+  private static function addChildren($dtdNode, $xmlNode) {
+    //self::addAttributes($dtdNode, $xmlNode);
 
-    $children = $this->parseContentSpecification($dtdNode->contentSpecification);
+    $children = self::parseContentSpecification($dtdNode->contentSpecification);
     $children = explode(',', $children);
     foreach($children as $child) {
       if(strlen($child) == 0) continue;
 
       if($child == '#PCDATA' or $child == 'ANY') {
-        $xmlNode->nodeValue = $this->stringGenerator();
+        $xmlNode->nodeValue = self::stringGenerator();
       } else if($child == 'EMPTY') {
         // Nothing
       } else {
-        $childNode = $this->dtdParser->elements[$child];
-        $element = $this->xmlTree->createElement($childNode->type);
+        $childNode = self::$dtdParser->elements[$child];
+        $element = self::$xmlTree->createElement($childNode->type);
         $xmlNode->appendChild($element);
-        $this->addChildren($childNode, $element);
+        self::addChildren($childNode, $element);
       }
     }
   }
 
-  public function writeXML() {
-    if($this->xmlTree->validate()) {
-      echo $this->xmlTree->saveXML()."\n";
+  private static function writeXML() {
+    if(self::$xmlTree->validate()) {
+      echo self::$xmlTree->saveXML()."\n";
     } else {
+      echo 'oops';
       // errors??
     }
   }
 
-  private function getQuantifierAction($s) {
+  private static function getQuantifierAction($s) {
     $limit = 49;
     $amount = -1;
 
@@ -141,14 +145,14 @@ class DTDtoXML {
     return $amount;
   }
 
-  private function parseContentSpecification($cs) {
+  private static function parseContentSpecification($cs) {
     $foundParens = preg_match('/\(([^(]*?)\)/', $cs, $matches, PREG_OFFSET_CAPTURE);
     if($foundParens === 0) {
       // Go through each element in the final list and check for individual quantifiers
       $elements = explode(',', $cs);
       foreach($elements as &$element) {
         $lastChar = substr($element, -1, 1);
-        $quantifierAction = $this->getQuantifierAction($lastChar);
+        $quantifierAction = self::getQuantifierAction($lastChar);
         if($quantifierAction >= 0) {
           $element = substr($element, 0, -1);
           $element = implode(',', array_fill(0, $quantifierAction, $element));
@@ -167,7 +171,7 @@ class DTDtoXML {
 
     // Evaluate quantifier
     $extraChar = 0; // If we need to remove an extra character when substringing
-    $quantifierAction = $this->getQuantifierAction($quantifier);
+    $quantifierAction = self::getQuantifierAction($quantifier);
     if($quantifierAction >= 0) {
       $extraChar = 1;
       // parsed = parsed * quantifier action
@@ -188,10 +192,10 @@ class DTDtoXML {
     $cs = substr_replace($cs, $innerEvaluted, $match[1] - 1, strlen($match[0]) + 2 + $extraChar);
 
     // Recurse until we have nothing more to parse
-    return $this->parseContentSpecification($cs);
+    return self::parseContentSpecification($cs);
   }
 
-  private function stringGenerator($minLength = 10, $maxLength = 20) {
-    return $this->faker->words($this->faker->numberBetween($minLength, $maxLength), true);
+  private static function stringGenerator($minLength = 10, $maxLength = 20) {
+    return self::$faker->words(self::$faker->numberBetween($minLength, $maxLength), true);
   }
 }
